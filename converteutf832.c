bf2804d96c4ffcdd168d18d3b6a4614bd70cb905
Davi-Donato-PUC <davidonato.puc@gmail.com>
2024-10-12 03:07:57 -0300

    F2

--- converteutf832.c
+++ converteutf832.c
@@ -34,3 +34,43 @@ void convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida){
         fwrite(&utf32, sizeof(unsigned int), 1,arquivo_saida);
     }
 }
+
+ int UTF32toUTF8(unsigned int utf32, unsigned char * utf8) {
+    if (utf32 <= 0x7F) {
+        utf8[0] = utf32 & 0x7F;
+        return 1;
+    } else if (utf32 <= 0x7FF) {
+        utf8[0] = 0xC0 | ((utf32 >> 6) & 0x1F);
+        utf8[1] = 0x80 | (utf32 & 0x3F);
+        return 2;
+    } else if (utf32 <= 0xFFFF) {
+        utf8[0] = 0xE0 | ((utf32 >> 12) & 0x0F);
+        utf8[1] = 0x80 | ((utf32 >> 6) & 0x3F);
+        utf8[2] = 0x80 | (utf32 & 0x3F);
+        return 3;
+    } else if (utf32 <= 0x10FFFF) {
+        utf8[0] = 0xF0 | ((utf32 >> 18) & 0x07);
+        utf8[1] = 0x80 | ((utf32 >> 12) & 0x3F);
+        utf8[2] = 0x80 | ((utf32 >> 6) & 0x3F);
+        utf8[3] = 0x80 | (utf32 & 0x3F);
+        return 4;
+    } else {
+        return -1;
+    }
+}
+
+int convUtf32p8(FILE *arquivoEntrada, FILE *arquivoSaida) {
+    unsigned int BOM;
+    fread(&BOM, 4, 1, arquivoEntrada);
+    unsigned int TamanhoUTF8;
+    unsigned int U32;
+    unsigned char * UTF8 = (unsigned char*) malloc(4);
+
+    while ( fread(&U32, 4, 1, arquivoEntrada) == 1 ) {
+        TamanhoUTF8 = UTF32toUTF8(U32, UTF8);
+
+        fwrite(UTF8, TamanhoUTF8, 1, arquivoSaida);
+        }
+
+    return 0;
+    }
--- testeconv.c
+++ testeconv.c
@@ -2,12 +2,19 @@
 #include "converteutf832.h"
 
 int main(){
-  FILE* fEntrada = fopen("utf8_grande","rb");
-  FILE* fSaida = fopen("utf32_peq_convertido.txt", "wb");
+    FILE* fEntrada = fopen("utf8_peq.txt","rb");
+    FILE* fSaida   = fopen("utf8to32_peq.txt", "wb");
+    FILE* fEntrada02 = fopen("utf32_peq.txt","rb");
+    FILE* fSaida02   = fopen("utf32to8_peq.txt", "wb");
 
-  convUtf8p32(fEntrada, fSaida);
+    convUtf8p32(fEntrada, fSaida);
+    convUtf32p8(fEntrada02, fSaida02);
 
-  fclose(fEntrada);
-  fclose(fSaida);
-  return 0;
+    fclose(fEntrada);
+    fclose(fSaida);
+    fclose(fEntrada02);
+    fclose(fSaida02);
+
+    return 0;
 }
+ 
